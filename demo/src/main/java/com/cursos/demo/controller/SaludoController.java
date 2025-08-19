021a7981a5af209faf897ac9e34ca0d3b2cf3f4e
María Bazán <formacionmbm@gmail.com>
2025-08-19 13:36:17 +0200

    Se añade un nuevo endpoint GET-/{nombre}

--- demo/src/main/java/com/cursos/demo/controller/SaludoController.java
+++ demo/src/main/java/com/cursos/demo/controller/SaludoController.java
@@ -1,6 +1,7 @@
 package com.cursos.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,11 @@ public class SaludoController {
 	@GetMapping
 	public String saludar() {
 		return "Hola mundo";
+	}
+	
+	@GetMapping("/{nombre}")
+	public String saludar(@PathVariable String nombre) {
+		return "Hola "+nombre;
 	}
 
 }
